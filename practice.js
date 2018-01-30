be5ea0e33f228992a4340fe1ac73fa2e277a5597
Tav Hafner <tav.hafner@gmail.com>
2018-01-30 14:35:49 -0700

    finished mainarray problems

--- practice.js
+++ practice.js
@@ -90,7 +90,17 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(nums) {
+  var emptyArr = [];
 
+  for (var i=0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      emptyArr.push(nums[i]);
+    }
+  }
+
+  return emptyArr;
+}
 
 
 ////////// EXTRA PRACTICE PROBLEMS BELOW //////////
